2f5c76c6ec25e746feda03557cba05c953748fa8
Graham McMillan <gmcmillan@protonmail.com>
2019-02-24 13:42:29 +0000

    Added another function

--- calculator.py
+++ calculator.py
@@ -9,3 +9,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def test_multiplication(self):
+    assert 100 == calculator.multiply(10, 10)
